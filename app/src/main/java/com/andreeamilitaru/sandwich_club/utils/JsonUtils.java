92936199c192e81c9c940c6c4aa7b61f19d0d2a0
Andreea Militaru <andreea.militaru88@gmail.com>
2018-08-29 15:43:25 -0700

    Add provided sample code

---
+++ app/src/main/java/com/andreeamilitaru/sandwich_club/utils/JsonUtils.java
@@ -0,0 +1,10 @@
+package com.andreeamilitaru.sandwich_club.utils;
+
+import com.andreeamilitaru.sandwich_club.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+}
