83e84062d1d501b9600eecc69d760b87361f0d8a
Andreea Militaru <andreea.militaru88@gmail.com>
2018-09-01 08:12:33 -0700

    Implement the JSON parsing in JsonUtils so it produces a Sandwich Object that can be used to populate the UI that you designed

--- app/src/main/java/com/andreeamilitaru/sandwich_club/utils/JsonUtils.java
+++ app/src/main/java/com/andreeamilitaru/sandwich_club/utils/JsonUtils.java
@@ -2,9 +2,44 @@ package com.andreeamilitaru.sandwich_club.utils;
 
 import com.andreeamilitaru.sandwich_club.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject sandwichJsonObject = new JSONObject(json);
+            JSONObject nameObject = sandwichJsonObject.getJSONObject("name");
+            String mainName = nameObject.getString("mainName");
+            JSONArray alsoKnownAsObject = nameObject.getJSONArray("alsoKnownAs");
+            String placeOfOrigin = sandwichJsonObject.getString("placeOfOrigin");
+            String description = sandwichJsonObject.getString("description");
+            String image = sandwichJsonObject.getString("image");
+            JSONArray ingredientsObject = sandwichJsonObject.getJSONArray("ingredients");
+
+            List<String> alsoKnownAs = jsonArrayToList(alsoKnownAsObject);
+            List<String> ingredients = jsonArrayToList(ingredientsObject);
+
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Read all values from the JSONArray into the ArrayList
+     */
+    private static List<String> jsonArrayToList(JSONArray jsonArray) throws JSONException {
+        List<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            arrayList.add(jsonArray.getString(i));
+        }
+        return arrayList;
     }
 }
